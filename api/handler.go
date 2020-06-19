d2a7220c3bcfd21aee63d862d30cb9909c53cea1
Pig Fang <g-plane@hotmail.com>
2020-06-19 09:20:34 +0800

    refactor

--- api/handler.go
+++ api/handler.go
@@ -6,6 +6,11 @@ import (
 	"github.com/ip2location/ip2location-go"
 )
 
+const (
+	domainNameGlobal   string = "https://littleskin.cn"
+	domainNameMainland string = "https://mcskin.littleservice.cn"
+)
+
 // Handler handles request.
 func Handler(w http.ResponseWriter, r *http.Request) {
 	db, err := ip2location.OpenDB("./IP2LOCATION-LITE-DB1.IPV6.BIN")
@@ -23,14 +28,17 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	// for debugging
 	headers.Add("X-IP-Location", result.Country_short)
 
-	domainName := ""
 	if result.Country_short == "CN" {
-		domainName = "https://mcskin.littleservice.cn"
+		makeResponse(domainNameMainland, w, r)
 	} else {
-		domainName = "https://littleskin.cn"
+		makeResponse(domainNameGlobal, w, r)
 	}
+}
 
+func makeResponse(domainName string, w http.ResponseWriter, r *http.Request) {
+	headers := w.Header()
 	headers.Set("Location", domainName+r.URL.String())
 	headers.Add("X-Authlib-Injector-API-Location", domainName+"/api/yggdrasil")
+
 	w.WriteHeader(http.StatusTemporaryRedirect)
 }
